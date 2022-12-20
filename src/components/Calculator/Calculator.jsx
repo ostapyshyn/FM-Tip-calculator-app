eac4959a8e7a0074bd7f6b7c9be8157828564f28
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2022-12-20 22:24:58 +0200

    Add basic styles

---
+++ src/components/Calculator/Calculator.jsx
@@ -0,0 +1,8 @@
+import React, { useRef } from 'react';
+import styles from './Calculator.module.scss';
+
+const Calculator = () => {
+  return <section>Splitter</section>;
+};
+
+export default Calculator;
