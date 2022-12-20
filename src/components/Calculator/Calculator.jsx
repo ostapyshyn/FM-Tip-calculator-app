19969a5bf64f183d7748cb8c2e8e4891c2fea674
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2022-12-20 22:46:09 +0200

    Add text

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -1,8 +1,24 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import styles from './Calculator.module.scss';
 
 const Calculator = () => {
-  return <section>Splitter</section>;
+  return (
+    <section className={styles.calculator}>
+      <div>
+        <p>Bill</p>
+        <input type="number" />
+        <p>Select Tip %</p>
+        <p>Number of People</p>
+        <input type="number" name="" id="" />
+      </div>
+
+      <div>
+        <p>Tip Amount / person</p>
+        <p>Total / person</p>
+        <button>Reset</button>
+      </div>
+    </section>
+  );
 };
 
 export default Calculator;
