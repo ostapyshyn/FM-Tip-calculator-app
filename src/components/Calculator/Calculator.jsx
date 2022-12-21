a1a36c2a336a5af7638f4d46cbfe13c2716d836e
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2022-12-21 22:28:27 +0200

    Add info

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import styles from './Calculator.module.scss';
+import logo from '../assets/logo.svg';
 
 const Calculator = () => {
   return (
-    <section className={styles.calculator}>
-      <div>
-        <p>Bill</p>
-        <input type="number" />
-        <p>Select Tip %</p>
-        <p>Number of People</p>
-        <input type="number" name="" id="" />
-      </div>
+    <>
+      <img src={logo} alt="logo" className={styles.logo} />
+      <section className={styles.calculator}>
+        <div className={styles.info}>
+          <p>Bill</p>
+          <input type="number" />
+          <p>Select Tip %</p>
+          <p>Number of People</p>
+          <input type="number" name="" id="" />
+        </div>
 
-      <div>
-        <p>Tip Amount / person</p>
-        <p>Total / person</p>
-        <button>Reset</button>
-      </div>
-    </section>
+        <div className={styles.result}>
+          <p>Tip Amount / person</p>
+          <p>Total / person</p>
+          <button>Reset</button>
+        </div>
+      </section>
+    </>
   );
 };
 
