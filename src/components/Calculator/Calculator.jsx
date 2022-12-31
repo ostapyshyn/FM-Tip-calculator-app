5eb16a63d9717b9798bd5fdcdd4dcd175a6756cb
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2022-12-31 23:10:32 +0200

    Fix buttons

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Calculator.module.scss';
 import logo from '../assets/logo.svg';
 import person from '../assets/icon-person.svg';
 import dollar from '../assets/icon-dollar.svg';
 
 const Calculator = () => {
+  const [percent, setPercent] = useState();
+
   return (
     <>
       <img src={logo} alt="logo" className={styles.logo} />
@@ -19,16 +21,41 @@ const Calculator = () => {
           <p className={styles.select_tip}>Select Tip %</p>
 
           <div className={styles.tips}>
-            <button className={styles.tip}>5 %</button>
-            <button className={styles.tip}>10 %</button>
-            <button className={styles.tip}>15 %</button>
-            <button className={styles.tip}>25 %</button>
-            <button className={styles.tip}>50 %</button>
+            <button
+              className={styles.tip}
+              onClick={() => setPercent(5)}
+              style={percent === 5 ? { background: '#26C2AE', color: '#00474B' } : null}>
+              5%
+            </button>
+            <button
+              className={styles.tip}
+              onClick={() => setPercent(10)}
+              style={percent === 10 ? { background: '#26C2AE', color: '#00474B' } : null}>
+              10%
+            </button>
+            <button
+              className={styles.tip}
+              onClick={() => setPercent(15)}
+              style={percent === 15 ? { background: '#26C2AE', color: '#00474B' } : null}>
+              15%
+            </button>
+            <button
+              className={styles.tip}
+              onClick={() => setPercent(25)}
+              style={percent === 25 ? { background: '#26C2AE', color: '#00474B' } : null}>
+              25%
+            </button>
+            <button
+              className={styles.tip}
+              onClick={() => setPercent(50)}
+              style={percent === 50 ? { background: '#26C2AE', color: '#00474B' } : null}>
+              50%
+            </button>
             <input className={styles.custom} type="text" name="" id="" placeholder="Custom" />
           </div>
 
           <p className={styles.name}>Number of People</p>
-          <div className={styles.input}>
+          <div className={styles.input} style={{ marginBottom: '0px' }}>
             <input type="text" name="" id="" placeholder="0" />
             <img src={person} alt="person" className={styles.dollar} />
           </div>
